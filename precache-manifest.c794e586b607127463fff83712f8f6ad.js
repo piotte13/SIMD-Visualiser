399e8bfdf8c9ec57f93a6faaa07c320fe8c1b0ba
piotte13 <jeremie.piotte@gmail.com>
2018-12-12 18:03:30 -0500

    Updates

---
+++ precache-manifest.c794e586b607127463fff83712f8f6ad.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b495fa123a7a11ae59268e42317cb39",
+    "url": "/SIMD-Visualiser/static/media/cryingboy.5b495fa1.svg"
+  },
+  {
+    "revision": "a08a6ab04f6a72d05e793d569072e8d4",
+    "url": "/SIMD-Visualiser/static/media/cog.a08a6ab0.svg"
+  },
+  {
+    "revision": "811c1a47ebf6e78000f88c01851c2c24",
+    "url": "/SIMD-Visualiser/static/media/c-programming.811c1a47.svg"
+  },
+  {
+    "revision": "f02ad007903e0c30d880",
+    "url": "/SIMD-Visualiser/static/js/runtime~main.f02ad007.js"
+  },
+  {
+    "revision": "83cb54f2f087a47a0fda",
+    "url": "/SIMD-Visualiser/static/js/main.83cb54f2.chunk.js"
+  },
+  {
+    "revision": "b6b91823c2ceef905511",
+    "url": "/SIMD-Visualiser/static/js/1.b6b91823.chunk.js"
+  },
+  {
+    "revision": "83cb54f2f087a47a0fda",
+    "url": "/SIMD-Visualiser/static/css/main.9f57a6d5.chunk.css"
+  },
+  {
+    "revision": "b6b91823c2ceef905511",
+    "url": "/SIMD-Visualiser/static/css/1.c0f6615b.chunk.css"
+  },
+  {
+    "revision": "80646dd80c19d4226fdc7084b9566841",
+    "url": "/SIMD-Visualiser/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/SIMD-Visualiser/precache-manifest.d4cf690aa239eec6325de28bc50ac258.js"
+  "/SIMD-Visualiser/precache-manifest.c794e586b607127463fff83712f8f6ad.js"
 );
 
 workbox.clientsClaim();
